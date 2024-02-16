7bfeb19499872c9fb62abd59a638a2d380461039
Magnus Holm Rasmussen <mara@cs.au.dk>
2024-02-16 16:41:02 +0100

    tried to fix the transformation

--- visualization.js
+++ visualization.js
@@ -4,21 +4,21 @@ document.addEventListener("DOMContentLoaded", () => {
   const ctx = canvas.getContext("2d");
 
   // This can be changed to scale the entire chart
-  LAYER_SIZE = 100
+  LAYER_SIZE = 30
 
   // the ratio between 1 value in an interval and 1 degree on the circle.
-  RATIO = 0 
-  
+  RATIO = 0
+
   // center position 
-  let x = canvas.width / 2 
+  let x = canvas.width / 2
   let y = canvas.height / 2
 
   // Function that draws a single 
   function drawNode(node) {
 
     // default color for first three layers. 
-    let color = "red" 
-    
+    let color = "red"
+
     // Gets a color based on the branch (from the third layer, and then the lightness is chosen by the depth)
     if (node.branchNr)
       color = get_color(node.branchNr, node.depth)
@@ -27,18 +27,18 @@ document.addEventListener("DOMContentLoaded", () => {
     if (node.status == "root") {
 
       // The ratio is the entire circles degrees devided between the roots interval
-      RATIO = (2*Math.PI) / (node.interval[1] - node.interval[0])
+      RATIO = (2 * Math.PI) / (node.interval[1] - node.interval[0])
 
       // draw a circle
       ctx.beginPath();
-      ctx.arc(x, y, LAYER_SIZE, 0, 2*Math.PI);
+      ctx.arc(x, y, LAYER_SIZE, 0, 2 * Math.PI);
       ctx.fillStyle = "black";
       ctx.fill();
       ctx.lineWidth = 0.01; // outline width
       ctx.strokeStyle = "#000"; // outline color
       ctx.stroke();
 
-      return 
+      return
     }
 
     if (RATIO == 0) {
@@ -48,28 +48,28 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Non-root nodes draws semi donuts
     ctx.beginPath()
-    ctx.fillStyle = color; 
+    ctx.fillStyle = color;
     // we draw two arcs spanning the nodes interval (normalized with the ratio), one layer-width apart and fill in the space 
-    ctx.arc(x, y, 2 * LAYER_SIZE + (LAYER_SIZE*(node.depth-1)), node.interval[0]*RATIO, node.interval[1]*RATIO, false); // outer 
-    ctx.arc(x, y, 1 * LAYER_SIZE + (LAYER_SIZE*(node.depth-1)), node.interval[1]*RATIO, node.interval[0]*RATIO, true); // inner 
+    ctx.arc(x, y, 2 * LAYER_SIZE + (LAYER_SIZE * (node.depth - 1)), node.interval[0] * RATIO, node.interval[1] * RATIO, false); // outer 
+    ctx.arc(x, y, 1 * LAYER_SIZE + (LAYER_SIZE * (node.depth - 1)), node.interval[1] * RATIO, node.interval[0] * RATIO, true); // inner 
     ctx.fill();
-    // the outline width is based on the interval size and the depth (so small intervals and deep nodes have lin outlines)
-    ctx.lineWidth = (node.interval[1]*RATIO - node.interval[0]*RATIO) * (5 * node.depth); 
+    // the outline width is based on the interval size
+    ctx.lineWidth = Math.max((node.interval[1] * RATIO - node.interval[0] * RATIO), 0.5);
     ctx.strokeStyle = "#000"; // outline color
     ctx.stroke();
   }
 
   // Returns a color based on the branch and depth
   function get_color(branchNr, depth) {
-    const base_colors = [ "#3498db", "#2ecc71", "#e74c3c", "#f39c12", "#9b59b6", "#1abc9c", "#34495e", "#d35400", "#c0392b", "#7f8c8d" ];
-    const color = base_colors[branchNr];
+    const base_colors = ["#3498db", "#2ecc71", "#e74c3c", "#f39c12", "#9b59b6", "#1abc9c", "#34495e", "#d35400", "#c0392b", "#7f8c8d"];
+    const color = base_colors[branchNr % 9];
     const adjustedColor = lightenColor(color, depth);
     return adjustedColor;
   }
 
   // Function to lighten a color
   function lightenColor(hexColor, lightness) {
-    scale = 10 // adjust how much the colors change with depth
+    scale = 2 // adjust how much the colors change with depth
     // Remove the '#' character if present
     hexColor = hexColor.replace(/^#/, '');
 
@@ -101,10 +101,10 @@ document.addEventListener("DOMContentLoaded", () => {
           drawNode(object);
           index++;
 
-          if (index % 30 === 0) {
-              setTimeout(drawNextNode, 0); 
+          if (index % 100 === 0) {
+            setTimeout(drawNextNode, 0);
           } else {
-              drawNextNode(); // Continue without delay
+            drawNextNode(); // Continue without delay
           }
         } catch (error) {
           console.error('Error parsing line:', line);
@@ -134,11 +134,10 @@ document.addEventListener("DOMContentLoaded", () => {
       })
       .catch(error => {
         console.error('Error reading file:', error);
-    });
-}
+      });
+  }
 
 
   readAndDrawNodesFromFile('transformed_data.txt');
-    
+
 });
-  
